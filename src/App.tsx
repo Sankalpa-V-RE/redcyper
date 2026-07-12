e63ecc52d2fefadeefb318d4f938ed0eb2b35352
Sankalpa-V-RE <vihangadharmarathna@gmail.com>
2026-07-12 21:48:31 +0530

    adding analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import BootSequence from './components/BootSequence'
 import Terminal from './components/Terminal'
 
@@ -6,15 +7,18 @@ function App() {
   const [booted, setBooted] = useState(false)
 
   return (
-    <div className="crt-container">
-      <div className="crt-content">
-        {!booted ? (
-          <BootSequence onComplete={() => setBooted(true)} />
-        ) : (
-          <Terminal />
-        )}
+    <>
+      <div className="crt-container">
+        <div className="crt-content">
+          {!booted ? (
+            <BootSequence onComplete={() => setBooted(true)} />
+          ) : (
+            <Terminal />
+          )}
+        </div>
       </div>
-    </div>
+      <Analytics />
+    </>
   )
 }
 
